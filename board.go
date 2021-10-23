ea4c75c2e408cbc6405730a06365381ae38bbfe7
Roman Hoog Antink <basman@disconnect.ch>
2021-10-24 00:43:26 +0200

    load boards from files

--- board.go
+++ board.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"strconv"
-	"strings"
+	"log"
+	"os"
+	"path/filepath"
 )
 
 var (
@@ -16,108 +18,7 @@ var (
 		&Difficulty{"Impossible", 16 * Minutes, 26},
 	}
 
-	DEFAULT_BOARD *Board = ParseBoard("ATC Standard", `
-        .....1....2.........3....
-        .........................
-        .........................
-        .........................
-        ..............+..........
-        4..............%.........
-        .........................
-        .........................
-        .........................
-        .........................
-        0....*...+=.........*...9
-        .........................
-        .........................
-        .........................
-        ........................7
-        .........................
-        .........................
-        .........................
-        .........................
-        .....8........5.....6....
-    `, `
-    # Format: weight: entry-exit-direction
-        6: 0-9-E  9-0-W
-        6: 1-8-S  8-1-N
-        6: 2-7-SE 7-2-NW
-        6: 3-6-S  6-3-N
-        6: 4-5-SE 5-4-NW
-
-        1: 0-=-E  1-=-S  2-=-SE 3-=-S  4-=-SE 5-=-NW 6-=-N  7-=-NW 8-=-N  9-=-W
-        1: 0-%-E  1-%-S  2-%-SE 3-%-S  4-%-SE 5-%-NW 6-%-N  7-%-NW 8-%-N  9-%-W
-        1: =-0-W  =-1-W  =-2-W  =-3-W  =-4-W  =-5-W  =-6-W  =-7-W  =-8-W  =-9-W
-        1: %-0-NW %-1-NW %-2-NW %-3-NW %-4-NW %-5-NW %-6-NW %-7-NW %-8-NW %-9-NW
-        2: =-=-W  %-%-NW =-%-W %-=-NW
-    `)
-
-	CROSSWAYS_BOARD *Board = ParseBoard("Crossways", `
-        .....4.........6.........8.....
-        ...............................
-        ...............................
-        ...............................
-        ...............................
-        ...............................
-        0.............................1
-        ...............................
-        ...............................
-        ...............................
-        ....+=.........*...............
-        ...............................
-        ...............................
-        ...............................
-        2.............................3
-        ...............................
-        ...............................
-        ...............................
-        ...............................
-        ...............................
-        .....5.........7.........9.....
-    `, `
-        4: 0-1-E 3-2-W
-        2: 4-5-S 5-4-N
-        2: 6-7-S 7-6-N
-        2: 8-9-S 9-8-N
-        2: 4-9-SE 9-4-NW
-        2: 8-5-SW 5-8-NE
-
-        2: 0-=-E  3-=-W
-        2: =-0-W  =-3-W
-        1: 4-=-SE 5-=-NE 6-=-S  7-=-N  8-=-SW  9-=-NW
-        1: =-4-W  =-5-W  =-6-W  =-7-W  =-8-W   =-9-W
-    `)
-
-	NOFLY_BOARD *Board = ParseBoard("NoFly Zone", `
-        ............5....6............
-        ..............................
-        ..............................
-        ..............................
-        ..............................
-        ..............................
-        .......xxxxxxxxxxxxxxxx.......
-        .......xxxxxxxxxxxxxxxx.......
-        1......xxxxxxxxxxxxxxxx......3
-        .......xxxxxxxxxxxxxxxx.......
-        .......xxxxxxxxxxxxxxxx.......
-        2......xxxxxxxxxxxxxxxx......4
-        .......xxxxxxxxxxxxxxxx.......
-        .......xxxxxxxxxxxxxxxx.......
-        ..............................
-        ..............................
-        ..............................
-        ..............................
-        ..............................
-        ..............................
-        ............7....8............
-    `, `
-        1: 1-3-E 4-2-W
-        1: 5-7-S 8-6-N
-    `)
-
-	BOARDS []*Board = []*Board{
-		DEFAULT_BOARD, CROSSWAYS_BOARD, NOFLY_BOARD,
-	}
+	BOARDS []*Board = LoadBoards("games/*.json")
 )
 
 type Board struct {
@@ -178,41 +79,71 @@ func (r Route) String() string {
 	return fmt.Sprintf("%s-%s", string(r.entry), string(r.exit))
 }
 
-func ParseBoard(name string, s string, rs string) *Board {
-	b := &Board{
-		name:        name,
-		entrypoints: make(map[rune]*EntryPoint),
-		navaids:     make([]Position, 0),
+func LoadBoards(filepattern string) []*Board {
+	boards := make([]*Board, 0)
+
+	flist, err := filepath.Glob(filepattern)
+	if err != nil {
+		log.Fatalf("could not list files in %v\n", filepattern)
+	}
+
+	for _, f := range flist {
+		boards = append(boards, ParseBoard(f))
+	}
+
+	return boards
+}
+
+func ParseBoard(filename string) *Board {
+	// transfer structs for loading and converting the JSON board
+	type fileRoute struct {
+		Weight    int
+		Entry     string
+		Exit      string
+		Direction string
+	}
+
+	type fileBoard struct {
+		Name   string
+		Board  []string
+		Routes []fileRoute
+	}
+
+	buf, err := os.ReadFile(filename)
+	if err != nil {
+		log.Fatalf("could not load board from file '%v'\n", filename)
+	}
+
+	var fBoard fileBoard
+	err = json.Unmarshal(buf, &fBoard)
+	if err != nil {
+		log.Fatalf("failed to unmarshal board from file '%v': %v\n", filename, err)
+	}
+
+	b := Board{
+		name:        fBoard.Name,
 		routes:      make([]Route, 0),
-		nofly:       make([]Position, 0),
+		entrypoints: make(map[rune]*EntryPoint, 0),
+		navaids:     make([]Position, 0),
+		nofly: 		 make([]Position, 0),
 	}
 
-	lines := make([]string, 0, 40)
-
-	for _, l := range strings.Split(s, "\n") {
-		l = strings.Trim(l, " \r\n")
-		if len(l) > 0 {
-			lines = append(lines, l)
-
-			if b.width == 0 {
-				b.width = len(l)
-			} else if b.width != len(l) {
-				fmt.Println(l, len(l), b.width)
-				panic("inconsistent width")
-			}
-		}
-	}
-
-	b.height = len(lines)
+	b.height = len(fBoard.Board)
 	if b.height <= 0 {
-		panic("board has no height")
+		log.Fatalln("board has no height")
 	}
 
-	for x := 0; x < b.width; x += 1 {
-		for y := 0; y < b.height; y += 1 {
+	// load board from file
+	for y, row := range fBoard.Board {
+		if b.width == 0 {
+			b.width = len(row)
+		} else if b.width != len(row) {
+			log.Fatalf("inconsistent width '%v': %v, expected %v\n", row, len(row), b.width)
+		}
+
+		for x, ch := range row {
 			pos := Position{x: x, y: y}
 
-			ch := lines[y][x]
 			switch ch {
 			case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
 				b.entrypoints[rune(ch)] = &EntryPoint{
@@ -225,7 +156,7 @@ func ParseBoard(name string, s string, rs string) *Board {
 				var dir Direction
 				for _, d := range DIRECTIONS {
 					pos2 := pos.Move(d, 1)
-					if lines[pos2.y][pos2.x] == '+' {
+					if fBoard.Board[pos2.y][pos2.x] == '+' {
 						dir = d
 					}
 				}
@@ -243,44 +174,29 @@ func ParseBoard(name string, s string, rs string) *Board {
 				b.nofly = append(b.nofly, pos)
 			case '.':
 			default:
-				panic("unknown spec: " + string(ch))
+				panic("unknown board spec: " + string(ch))
 			}
 		}
 	}
 
-	// TODO: better parsing
-	for _, l := range strings.Split(rs, "\n") {
-		l = strings.Trim(l, " \r\n")
-		if l == "" || l[0] == '#' {
-			continue
+	// load routes
+	for _, r := range fBoard.Routes {
+		entry := rune(r.Entry[0])
+		exit := rune(r.Exit[0])
+
+		_, ok_entry := b.entrypoints[entry]
+		_, ok_exit := b.entrypoints[exit]
+		if !ok_entry || !ok_exit {
+			panic("unknown entrypoint: " + string(r.Entry) + " or " + string(r.Exit))
 		}
 
-		parts := strings.SplitN(l, ":", 2)
-		weight, _ := strconv.Atoi(parts[0])
-		routes := strings.Split(parts[1], " ")
-		for _, r := range routes {
-			if r == "" {
-				continue
-			}
-
-			r_parts := strings.SplitN(r, "-", 3)
-
-			route := Route{
-				entry:     rune(r_parts[0][0]),
-				exit:      rune(r_parts[1][0]),
-				Direction: ParseDirection(r_parts[2]),
-				weight:    weight,
-			}
-			_, ok_entry := b.entrypoints[route.entry]
-			_, ok_exit := b.entrypoints[route.exit]
-			if !ok_entry || !ok_exit {
-				panic("unknown entrypoint: " + string(route.entry) + " or " + string(route.exit))
-			}
-			b.routes = append(b.routes, route)
-
-		}
-
+		b.routes = append(b.routes, Route{
+			entry:     entry,
+			exit:      exit,
+			weight:    r.Weight,
+			Direction: ParseDirection(r.Direction),
+		})
 	}
 
-	return b
+	return &b
 }
